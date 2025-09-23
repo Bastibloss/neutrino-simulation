d4049d403be1d8e9e11cf4feee514b12917f2d0a
Bastibloss <bastianpatovalenzuela@gmail.com>
2025-09-22 23:14:06 -0300

    Ultima version final boss
    Se arreglaron errores en el promedio y detalles para un mejor funcionamiento.

--- simulate.sh
+++ simulate.sh
@@ -1,25 +1,25 @@
 #!/bin/bash
 
-start=$(date +$s)
+start=$(date +%s%N)
 
-# Limpiar residuos
+#Limpiar residuos
 rm -f *.txt
 
 # Bucle de deteccion
-for I in $(seq 1 1 1000); do 
-	tiempo=$(date +"%Y-%m-%d %H:%M:%S") # Fecha y hora exacta
-	neutrinos=$((RANDOM % 11) # Eleccion al azar de cantidad de neutrinos en los eventos(0-10)
-	echo "Tiempo: $tiempo - Neutrinos" > "{I}.txt" 
+for I in $(seq 1 1 1000); do
+        tiempo=$(date +"%Y-%m-%d %H:%M:%S") # Fecha y hora exacta
+        neutrinos=$((RANDOM % 11)) # Eleccion al azar de cantidad de neutrinos en los eventos(0-10)
+        echo "Tiempo: $tiempo - Neutrinos: $neutrinos" > "${I}.txt"
 done
-# Compactar en results.txt
+# reducir en results.txt
 cat *.txt > results.txt
 
-end=$(date +%s)
-runtime=$((end - start))
+end=$(date +%s%N)
+runtime=$(awk "BEGIN {print ($end - $start)/1000000000}")
 
-#calculamos el promedio (tiempo total/1000 eventos)
-promedio=$(echo "scale=4: $runtime / 1000")
+# calculamos el promedio (tiempo total/1000 eventos)
+promedio=$(awk -v r="$runtime" "BEGIN {print r/1000}")
 
-#guardar informacion de la simulacion
-echo "Tiempo total de la ejecucion:$runtime segundos" > performance.txt
-echo "Tiempo promedio por evento:$promedio segundos" >> performance.txt
+# guardar informacion de la simulacion
+echo "Tiempo total de la ejecucion: $runtime segundos" > performance.txt
+echo "Tiempo promedio por evento: $promedio segundos" >> performance.txt
