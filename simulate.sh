c904390d2ce2d5c380fc3309a0e704fd76a4be82
Bastibloss <bastianpatovalenzuela@gmail.com>
2025-09-22 22:48:26 -0300

    Update simulate.sh

--- simulate.sh
+++ simulate.sh
@@ -2,23 +2,23 @@
 
 start=$(date +$s)
 
-#Limpiar residuos
+# Limpiar residuos
 rm -f *.txt
 
-#Bucle de deteccion
+# Bucle de deteccion
 for I in $(seq 1 1 1000); do 
-	tiempo=$(date +"%Y-%m-%d %H:%M:%S") #Fecha y hora exacta
-	neutrinos=$((RANDOM % 11) #Eleccion al azar de cantidad de neutrinos en los eventos(0-10)
+	tiempo=$(date +"%Y-%m-%d %H:%M:%S") # Fecha y hora exacta
+	neutrinos=$((RANDOM % 11) # Eleccion al azar de cantidad de neutrinos en los eventos(0-10)
 	echo "Tiempo: $tiempo - Neutrinos" > "{I}.txt" 
 done
-#Compactar en results.txt
+# Compactar en results.txt
 cat *.txt > results.txt
 
 end=$(date +%s)
-rundtime=$((end - start))
+runtime=$((end - start))
 
 #calculamos el promedio (tiempo total/1000 eventos)
-Promedio=$(echo "scale=4: $runtime / 1000")
+promedio=$(echo "scale=4: $runtime / 1000")
 
 #guardar informacion de la simulacion
 echo "Tiempo total de la ejecucion:$runtime segundos" > performance.txt
